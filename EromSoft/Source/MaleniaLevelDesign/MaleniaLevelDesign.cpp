2a5afa191a4ca2acfbd4e92521c62ec89ff4cc11
seoseokkyo <seoseokkyo@naver.com>
2024-12-02 23:52:06 +0900

    Late Upload_25

---
+++ EromSoft/Source/MaleniaLevelDesign/MaleniaLevelDesign.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MaleniaLevelDesign.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MaleniaLevelDesign, "MaleniaLevelDesign" );
+ 
---
+++ EromSoft/Source/MaleniaLevelDesign/MaleniaLevelDesignGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaleniaLevelDesignGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMaleniaLevelDesignGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMaleniaLevelDesignGameMode();
+};
+
+
+
